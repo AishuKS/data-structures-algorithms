f356f7f8210f906233cbbba3c201b590a6ca3a7f
AishwaryaKandasamy <15aish98@gmail.com>
2025-04-03 18:17:28 -0500

    postorder traversal

---
+++ Trees/postorder_traversal.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+#preorder traversal
+def preorder(root):
+    if root is None:
+        return
+    
+    preorder(root.left)      
+    preorder(root.right)     
+    answer.append(root.data)
+
+answer = []
+preorder(root)
+print(answer)
