2c5bc964bbc88b9865f53201bb2bc39eb672de36
AishwaryaKandasamy <15aish98@gmail.com>
2025-07-12 00:25:50 -0500

    code change

--- Dynamic Programming Concepts/2) longest_common_subsequence.py
+++ Dynamic Programming Concepts/2) longest_common_subsequence.py
@@ -27,7 +27,7 @@ text2 = "ace"
 l1 = len(text1)
 l2 = len(text2)
 dp = [[0]*(l2+1) for i in range(l1+1)]
-print(dp)
+
 for i in range(1, l1+1):
     for j in range(1, l2+1):
         if text1[i-1] == text2[j-1]:
@@ -35,5 +35,5 @@ for i in range(1, l1+1):
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-print(dp)
+
 print(dp[l1][l2])
